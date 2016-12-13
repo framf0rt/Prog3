fa640ab6fd3a3614a52024b2b645d28083a0239b
framf0rt <anders@ekendahl.one>
2016-12-13 14:02:16 +0100

    Added renderer

--- GameProject/GameEngine.cpp
+++ GameProject/GameEngine.cpp
@@ -6,9 +6,13 @@
 int main(int argvc, char* argv[]) {
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_Window* win = SDL_CreateWindow("GameEngine", 100, 100, 500, 500, 0);
+	SDL_Renderer* ren = SDL_CreateRenderer(win, -1, 0);
+	
+	
 
 	SDL_Delay(5000);
 
+	SDL_DestroyRenderer(ren);
 	SDL_DestroyWindow(win);
 	SDL_Quit();
 	return 0;
