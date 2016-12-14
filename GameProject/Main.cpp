b678da05a4e9f339861738a216a082e4bb973744
Dannord <dnordberg86@gmail.com>
2016-12-14 09:35:42 +0100

    main

---
+++ GameProject/Main.cpp
@@ -0,0 +1,12 @@
+#include "GameEngine.h"
+#include <iostream>
+using namespace engine;
+
+int main(int argvc, char* argv[]) {
+	ge.run();
+	std::cout << "kan vi commita main nu?" << std::endl;
+
+
+
+	return 0;
+}
