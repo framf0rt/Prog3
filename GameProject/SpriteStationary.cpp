144bc732afb3d9b05047efd78634510cea07c9f5
framf0rt <anders@ekendahl.one>
2017-01-13 14:28:24 +0100

    Collision

--- GameProject/SpriteStationary.cpp
+++ GameProject/SpriteStationary.cpp
@@ -2,12 +2,29 @@
 
 namespace engine {
 
-	SpriteStationary* SpriteStationary::getInstance(const SDL_Rect& r, std::string path) {
-		return new SpriteStationary(r, path);
+	
+
+	SpriteStationary* SpriteStationary::getInstance(const SDL_Rect& r, std::string path, bool isGround, bool isBounceable, bool isKillZone) {
+		return new SpriteStationary(r, path, isGround, isBounceable, isKillZone);
 	}
 
-	SpriteStationary::SpriteStationary(const SDL_Rect& r, std::string path):Sprite(r, path)
+	SpriteStationary::SpriteStationary(const SDL_Rect& r, std::string path, bool isGround, bool isBounceable, bool isKillZone):Sprite(r, path)
 	{
+		this->isBounceable = isBounceable;
+		this->isGround = isGround;
+		this->isKillZone = isKillZone;
+	}
+
+	SDL_Rect SpriteStationary::getCollider() {
+		SDL_Rect A = getRect();
+
+		if (isGround == true) {
+			boxCollider = { A.x, A.y + 10, A.w, 10 };
+		}
+		if (isGround == false) {
+			boxCollider = { A.x,A.y,A.w,A.h };
+		}
+		return boxCollider;
 	}
 
 	void SpriteStationary::draw() {
